f0a78ec08b5b68c172ceeb9cda4fe90e942d8353
Maria Tanchyn <mariatanshyn@gmail.com>
2023-04-25 17:35:04 +0200

    added bank account input

--- pages/createInvoice/paymentActions.js
+++ pages/createInvoice/paymentActions.js
@@ -8,9 +8,14 @@ import styles from "./createInvoice.module.css"
 const PaymentActions = () => {
     return (
         <div className={styles.paymentActions}>
-        {/*    select currency, select payment method, select bankAccount, input paid, input left to pay, checkBox paid*/}
-            <SelectWithUnderline options={currencies} label={"Currency"} placeholder={"Currency"} />
+            {/*    select currency, select payment method, select bankAccount, input paid, input left to pay, checkBox paid*/}
+            <SelectWithUnderline options={currencies} label={"Currency"} placeholder={"Currency"}/>
             <SelectWithUnderline options={paymentMethods} label={"Payment Methods"} placeholder={"Payment Methods"}/>
+
+            <div className={styles.bankAccLabel}>
+                <span style={{fontSize:".9em", color: "rgba(27, 27, 27, 0.5)"}}>Bank Account </span>
+                <span>92 3842 3232 3203 9132 0000 3422 <hr/></span>
+            </div>
         </div>
     )
 }
